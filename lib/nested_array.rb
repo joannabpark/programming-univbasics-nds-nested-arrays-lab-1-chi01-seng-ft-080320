8f0310364f30862e65b1aced3b9fe662619ef8ed
JoAnna Park <joannabpark1@gmail.com>
2020-07-17 02:00:59 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,7 +31,7 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
-  sorted_matrix[ORGANIC_PRODUCE]["potatoes"]
+  matrix_lookup = sorted_matrix[ORGANIC_PRODUCE]["potatoes"]
 end
 
 def matrix_update(matrix, row, column, new_value)
